d0e1fba3d485c2812e9ce872d795dfa654b30443
agnieve70 <82817691+agnieve70@users.noreply.github.com>
2022-01-16 14:56:23 +0800

    added collectors fields and agents field in getAgetName

--- app/Http/Controllers/Api/CollectorController.php
+++ app/Http/Controllers/Api/CollectorController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+
 use Illuminate\Http\Request;
 
-class AgentController extends Controller
+class CollectorController extends Controller
 {
-    public function getAgentsName()
+    public function getCollector()
     {
-        $agents = User::select('id', 'name', 'email', 'contact')->where("role_id", '2')->get();
+        $agents = User::select('id', 'name', 'email', 'contact')->where("role_id", '3')->get();
         return response()->json([
             "status" => 1,
-            "message" => "Agents List",
+            "message" => "Collectors List",
             "data" => $agents
         ], 200);
     }
